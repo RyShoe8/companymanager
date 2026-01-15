b9741813868c7294097a95fb06854e5be7555db5
RyShoe8 <30275036+RyShoe8@users.noreply.github.com>
2026-01-15 05:16:58 -0600

    Remove manual head tag - Next.js App Router uses metadata API for favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,11 +82,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/images/icon.png" />
-        <link rel="shortcut icon" href="/images/icon.png" />
-        <link rel="apple-touch-icon" href="/images/icon.png" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
